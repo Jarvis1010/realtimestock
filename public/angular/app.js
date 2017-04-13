fb07864f661063c0ce27ea1ae1d51e6269ebfe74
Justin Travis Mair <travis.mair@gmail.com>
2017-04-13 15:30:13 +0000

    pulling in historical Data from Yahoo YQL API

--- public/angular/app.js
+++ public/angular/app.js
@@ -2,16 +2,18 @@ var app = angular.module('realtimestock',[]);
 
 app.controller('StockController',StockController);
 
-function StockController($scope){
+function StockController($scope,$http){
     var socket =io.connect(window.location.href);
     socket.on('add',(data)=>{
         $scope.stocks=data;
         $scope.$apply();
+        getStocks();
     });
     
     socket.emit('join');
     
     $scope.stocks=[];
+    $scope.stockData=[];
     
     $scope.onSubmit=function(){
        var found=$scope.stocks.find((stock)=>{
@@ -19,7 +21,7 @@ function StockController($scope){
        });
        
        if(!found){
-        $scope.stocks.push($scope.stockToAdd);
+        $scope.stocks.push($scope.stockToAdd.toUpperCase());
         socket.emit('add',$scope.stocks);
         $scope.stockToAdd='';
        }
@@ -30,5 +32,26 @@ function StockController($scope){
         socket.emit('add',$scope.stocks);
     };
     
+    function getStocks(){
+        $scope.stocks.forEach((symbol)=>{
+            getData(symbol);
+        });
+    }
+    
+    function getData(symbol){
+        let startDate="2016-11-01";
+        let endDate="2017-04-01";
+        let url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20=%20%22"
+        +symbol+
+        "%22%20and%20startDate%20=%20%22"
+        +startDate+
+        "%22%20and%20endDate%20=%20%22"
+        +endDate+
+        "%22&env=store://datatables.org/alltableswithkeys&format=json&callback=";
+        console
+        $http.get(url).then((results)=>{
+            console.log(results.data);
+        });
+    }
 }
 
